50a2a272a066c75bcc42d92ebf760bd4cdb38dc7
Zahid Khan <zahidk.8850@gmail.com>
2025-08-29 16:56:54 +0530

    updates to the sys args 2

--- codes_2/sys-args-2.py
+++ codes_2/sys-args-2.py
@@ -1,13 +1,13 @@
 import sys
 
 l = len(sys.argv)
-small = 0
-large = 0
+small = float(sys.argv[1])
+large = float(sys.argv[1])
 
 for i in range (1, l):
     if(float(sys.argv[i]) > large):
         large = float(sys.argv[i])
-    else:
+    elif(float(sys.argv[i])< small):
         small = float(sys.argv[i])
 
 print(small)
